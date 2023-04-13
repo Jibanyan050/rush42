a8e879061b8ba7a48f2022c02633b39b87bd05a5
42Schoolsace <130516676+42Schoolsace@users.noreply.github.com>
2023-04-13 13:35:46 +0930

    Update and rename land.js to landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,10 +1,16 @@
 const images = [
     "https://www.un.org/development/desa/disabilities/wp-content/uploads/sites/15/2019/07/E-SDGs-Poster-801x476.png",
     "https://www.floraandfauna.com.au/media/wysiwyg/What-Are-The-17-Sustainable-Development-Goals.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-WKfq8ejlr611Q6yW5A5Mj-FQeie3ElzkQ&usqp=CAU"
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Yon7X4ne2jv4JQEDQ4DnmmULuBVBNnIsFw&usqp=CAU",
+    "https://www.utrgv.edu/sustainability/_files/images/slides/unsdgs-banner.jpg"
   ];
+  
   let currentImageIndex = 0;
+  
   function changeBackground() {
     currentImageIndex = (currentImageIndex + 1) % images.length;
     document.body.style.backgroundImage = "url('" + images[currentImageIndex] + "')";
   }
+  
+  setInterval(changeBackground, 5000);
+  
